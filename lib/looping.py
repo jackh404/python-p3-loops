63148bf94c0a904e3f2519c78964c5c7b2d06857
Jack Henza <jackhenza@gmail.com>
2023-11-17 12:07:37 -0500

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i > 0:
+        print(i)
+        i -= 1
+    print('Happy New Year!')
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num**2 for num in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i=1
+    while i < 101:
+        if i % 3 and i % 5:
+            print(i)
+        elif i % 5:
+            print('Fizz')
+        elif i % 3:
+            print('Buzz')
+        else:
+            print('FizzBuzz')
+        i += 1
